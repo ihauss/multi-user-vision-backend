c3a9aedf968fe7c11852d6c57c5798217394445b
ihauss <ismael.hauss@gmail.com>
2026-04-05 10:30:57 +0700

    add database

--- API/app/main.py
+++ API/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from app.database import engine
+
+from app.models import User, Camera, CameraUser
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
 
 @app.get("/")
 def root():
---
+++ API/app/models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .camera import Camera
+from .camera_user import CameraUser
